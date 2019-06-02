2c1bdf001c4284e1ac9c36672d5eb5ada3f3747d
macibor811@student.polsl.pl <macibor811@student.polsl.pl>
2019-06-02 19:57:35 +0200

    Add SSL support

--- RepairManagementDesktop/src/main/java/pl/polsl/repairmanagementdesktop/utils/ConfiguredClientFactory.java
+++ RepairManagementDesktop/src/main/java/pl/polsl/repairmanagementdesktop/utils/ConfiguredClientFactory.java
@@ -1,7 +1,19 @@
 package pl.polsl.repairmanagementdesktop.utils;
 
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContexts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import pl.polsl.repairmanagementdesktop.RestErrorHandler;
@@ -9,18 +21,63 @@ import uk.co.blackpepper.bowman.ClientFactory;
 import uk.co.blackpepper.bowman.Configuration;
 import uk.co.blackpepper.bowman.RestTemplateConfigurer;
 
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
 @org.springframework.context.annotation.Configuration
 public class ConfiguredClientFactory {
+
 
     @Bean
     public ClientFactory configuredClient(@Value("${server.address}") String server, RestErrorHandler handler) {
         ClientFactory factory = Configuration.builder()
-                .setRestTemplateConfigurer(restTemplate -> restTemplate.setErrorHandler(handler))
+                .setRestTemplateConfigurer(restTemplate -> {
+                    restTemplate.setRequestFactory(sslRequestFactory());
+                    restTemplate.setErrorHandler(handler);
+                })
                 .setBaseUri(server + "/api")
                 .build()
                 .buildClientFactory();
         return factory;
     }
+
+
+    private HttpComponentsClientHttpRequestFactory sslRequestFactory(){
+        TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
+        SSLContext sslContext = null;
+        try {
+            sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext,
+                NoopHostnameVerifier.INSTANCE);
+
+        Registry<ConnectionSocketFactory> socketFactoryRegistry =
+                RegistryBuilder.<ConnectionSocketFactory> create()
+                        .register("https", sslsf)
+                        .register("http", new PlainConnectionSocketFactory())
+                        .build();
+
+        BasicHttpClientConnectionManager connectionManager =
+                new BasicHttpClientConnectionManager(socketFactoryRegistry);
+        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf)
+                .setConnectionManager(connectionManager).build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory =
+                new HttpComponentsClientHttpRequestFactory(httpClient);
+        return requestFactory;
+
+    }
+
+
 
 
 }
