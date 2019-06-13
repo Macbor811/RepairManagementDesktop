640c6c3450632d0a57046d04b30a287a462dc423
yamaz0 <szulc.patryk1c@gmail.com>
2019-06-13 22:13:08 +0200

    items
    wyswietlanie

---
+++ RepairManagementDesktop/src/main/java/pl/polsl/repairmanagementdesktop/model/item/ItemTableRow.java
@@ -0,0 +1,40 @@
+package pl.polsl.repairmanagementdesktop.model.item;
+
+
+import pl.polsl.repairmanagementdesktop.model.customer.CustomerEntity;
+import pl.polsl.repairmanagementdesktop.model.itemtype.ItemTypeEntity;
+
+public class ItemTableRow {
+
+    private String id;
+    private String name;
+    private ItemTypeEntity type;
+    private CustomerEntity client;
+
+    public ItemTableRow(ItemEntity entity){
+        String uriString =  entity.getUri().toString();
+
+        //processes URI string to get resource index(ex. api/customer/3)
+        this.id = uriString.substring(uriString.lastIndexOf("/") + 1);
+
+        this.name = entity.getName();
+        this.type = entity.getItemType();
+        this.client = entity.getOwner();
+
+    }
+
+
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public ItemTypeEntity getItemType() {
+        return type;
+    }
+    public CustomerEntity getOwner() {
+        return client;
+    }
+
+}
