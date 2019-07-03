545b29f5c7a79f7e3c7b351131efeb1121b685d7
macibor811@student.polsl.pl <macibor811@student.polsl.pl>
2019-07-03 20:09:53 +0200

    Create finalization service

--- RepairManagementDesktop/src/main/java/pl/polsl/repairmanagementdesktop/model/request/RequestService.java
+++ RepairManagementDesktop/src/main/java/pl/polsl/repairmanagementdesktop/model/request/RequestService.java
@@ -2,7 +2,9 @@ package pl.polsl.repairmanagementdesktop.model.request;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import pl.polsl.repairmanagementdesktop.FinalizationData;
 import pl.polsl.repairmanagementdesktop.utils.search.SearchQuery;
 import uk.co.blackpepper.bowman.Client;
 import uk.co.blackpepper.bowman.ClientFactory;
@@ -33,12 +35,35 @@ public class RequestService {
     }
     public RequestEntity findById(String id){
         String baseUriStr = client.getBaseUri().toString();
-        return client.get(URI.create(baseUriStr + "/" + id));
+        return client.get(UriComponentsBuilder.fromUri(
+                client.getBaseUri())
+                .path("/{id}")
+                .buildAndExpand(id).toUri());
     }
 
 
     public Page<RequestEntity> findAllMatching(SearchQuery query, int page, int size){
         URI uri = UriComponentsBuilder.fromUri(client.getBaseUri()).query(query.getQueryString()).build().toUri();
         return client.getPage(uri, page, size);
+    }
+
+    public void finalize(String id, String result, String status){
+        RestTemplate template = new RestTemplate();
+
+        var data = new FinalizationData();
+        data.setResult(result);
+        data.setStatus(status);
+
+        String baseUriStr = client.getBaseUri().toString();
+        //template.put(URI.create(baseUriStr + "/" + id + "/finalize"), data);
+
+        template.put(
+                UriComponentsBuilder.fromUri(
+                client.getBaseUri())
+                .path("/{id}/finalize")
+                .buildAndExpand(id).toUri(),
+                data
+        );
+
     }
 }
